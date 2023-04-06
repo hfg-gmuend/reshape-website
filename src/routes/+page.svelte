1b597e7dcf0a1dd73b8a32ceda3a5f46f6bdd1e4
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-04-06 13:49:15 +0200

    Add fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Welcome to reshape - Forum for Artificial intelligence in art an design</h1>
+<nav class="px-6 border-y-2 border-black h-11 flex items-center">
+	<span class="font-medium text-base">reshape</span>
+</nav>
+
+<main class="px-6">
+	<h1>Welcome to reshape - Forum for Artificial intelligence in art an design</h1>
+</main>
