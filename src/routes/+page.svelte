5d12a9b706b662ef517b332ee6a059db8b78b406
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-04-06 14:17:30 +0200

    Add mobile menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
-<nav class="px-6 border-y-2 border-black h-11 flex items-center">
-	<span class="font-medium text-base">reshape</span>
-</nav>
+<script>
+	import MenuIcon from '../lib/menu-icon.svelte';
+	let menuOpen = false;
 
-<main class="px-6">
-	<h1>Welcome to reshape - Forum for Artificial intelligence in art an design</h1>
-</main>
+	function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
+</script>
+
+<header class="fixed w-full z-10 px-6 border-y-2 border-black h-11 flex items-center">
+	<span class="font-medium text-base">reshape</span>
+	<button class="ml-auto p-2" on:click={toggleMenu} class:rotate-45={!menuOpen}><MenuIcon /></button
+	>
+</header>
+
+{#if menuOpen}
+	<nav class="fixed w-full h-screen pt-11 bg-reshape-mauve px-6 text-4xl">
+		<ul>
+			<li>Programm</li>
+			<li>Anmeldung</li>
+			<li>AI+D Lab</li>
+			<li>Kontakt</li>
+		</ul>
+	</nav>
+{/if}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			'reshape-mauve': '#ACACFF'
+		},
 		extend: {}
 	},
 	plugins: []
