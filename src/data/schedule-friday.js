1b545812f7d71f9f332509071b69cbde0b3779ea
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-04-20 21:24:47 +0200

    Update programme

--- src/data/schedule-friday.js
+++ src/data/schedule-friday.js
@@ -3,13 +3,13 @@ export default [
 		timeslot: '09:30-10:30',
 		title: 'Simon Maris',
 		subtitle: 'Talk (tbd)',
-		description: `tbd`
+		description: `Simon Maris is currently Professor (by proxy ~ in Vertretung) of AI - Design and Sustainability at the Department of Art and Design at the University of Applied Sciences Trier. He explores, questions, makes. Trained as an architect, he has long been fascinated by technology and it's intersection with society and art, with a particular focus on computational design, digital fabrication, robotics and more recently AI systems. He co-hosts the towards co-creation podcast.`
 	},
 	{
 		timeslot: '10:30-11:30',
 		title: 'Tom White',
 		subtitle: 'Machine Abstractions',
-		description: `tbd`
+		description: `Tom White (dribnet) is a New Zealand based artist investigating machine perception. His current work focuses on creating physical artworks that highlight how machines “see” and thus how they think, suggesting that these systems are capable of abstraction and conceptual thinking. He has exhibited computer based artwork internationally over the past 25 years with themes of artificial intelligence, interactivity, and computational creativity. He is currently a lecturer and researcher at University of Wellington School of Design where he teaches students the creative potential of computer programming and artificial intelligence.`
 	},
 	{
 		timeslot: '12:00-13:00',
