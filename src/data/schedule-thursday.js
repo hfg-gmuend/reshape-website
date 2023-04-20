8543ddb895f18636ac6a0a6936beaffe3ceaa3fe
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-04-20 18:58:50 +0200

    Update programme

--- src/data/schedule-thursday.js
+++ src/data/schedule-thursday.js
@@ -3,19 +3,21 @@ export default [
 		timeslot: '09:30-10:30',
 		title: 'Nadia Piet',
 		subtitle: 'Talk (tbd)',
-		description: 'tbd'
+		description: `Nadia Piet is a designer /researcher / creative with a focus on AI/ML, data, tech, (digital) culture and creativity.
+		She is the founder of AIxDESIGN, Head of Creative Technology at DEPT Agency, holds an MA in Data-Driven Design, and continues to explore through freelance and self-initiated projects & play. Her work questions how we design, interact, and relate to technology.
+		Previously, she worked as a design researcher for emerging tech with Bit, facilitated workshops to demystify digital with DECODED, published the AI meets Design toolkit in collaboration with MOBGEN | Fjord | Accenture Interactive, freelanced for 8+ years, and had the opportunity to live & work out of 10 countries across the globe.`
 	},
 	{
 		timeslot: '10:30-11:30',
 		title: 'Catherine Breslin',
 		subtitle: 'Conversational Design for Voice Assistants',
-		description: 'tbd'
+		description: `Catherine Breslin is a machine learning scientist and consultant based in Cambridge UK, and founder of Kingfisher Labs consulting. Since completing her PhD at the University of Cambridge in 2008, she has commercial and academic experience of automatic speech recognition, natural language understanding and human-computer dialogue systems, having previously worked at at Cambridge University, Toshiba Research, Amazon Alexa, and Cobalt Speech. She is excited by the application of research to real-world problems involving speech and language at scale.`
 	},
 	{
 		timeslot: '12:00-13:00',
 		title: 'Ploipailin Flynn',
 		subtitle: 'Talk (tbd)',
-		description: 'tbd'
+		description: `Ploipailin Flynn is a business and product strategist interested in the design and build of radically equitable and antiracist futures. She got tired of well-designed tech products and services that turned "accidentally" racist, so she launched ANTI ( antiracistby.design ) as a place of reference for business and product leads to build things that are antiracist by design. Her areas of interest include: The relationship between revenue models and discriminatory design. Antiracist and decolonial futures. Translating abstract principles into tangible products and services. Researching the intersectional impacts of Big Tech, AI, capitalism. `
 	},
 	{
 		timeslot: '13:00-14:00',
@@ -26,7 +28,7 @@ export default [
 		timeslot: '14:00-15:00',
 		title: 'Emily Saltz',
 		subtitle: 'Talk (tbd)',
-		description: 'tbd'
+		description: `Emily Saltz is a UX Researcher at Google Jigsaw, working on tools for platforms and moderators to address online harms. Before that, she was a UX Researcher at the New York Times R&D Lab, conducting research on topics ranging from media credibility (the News Provenance Project), to NLP Q&A tools. She was a 2020 Fellow at the Partnership on AI studying manipulated and synthetic media, where she co-authored "The Responsible AI Art Field Guide." She holds a Master’s in Computer-Interaction from Carnegie Mellon, and a BA in Linguistics from UC Santa Cruz, and is Part-Time Faculty in Design & Technology at Parsons (NYC). `
 	},
 	{
 		timeslot: '15:00-16:30',
@@ -39,6 +41,6 @@ export default [
 		timeslot: '17:00-17:45',
 		title: 'Bjørn Karmann (oio)',
 		subtitle: 'Intelligence as a material',
-		description: `tbd`
+		description: `Designing interactions for future products and experience. Living in Amsterdam and currently working as senior designer at oio studio.`
 	}
 ];
